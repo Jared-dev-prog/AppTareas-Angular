66d668dbcb378881c3d7baa03b6fdf1bca2a3378
Jared <jared.trejo@axity.com>
2023-04-13 11:53:39 -0600

    Deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { ITask } from './models/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AppTareas';
+
+  nameTask: string = '';
+  tasks: ITask[] = [];
+
+  constructor() {}
+
+  // Add a new task
+  addNewTask(): void {
+    const newTask: ITask = {
+      name: this.nameTask,
+      state: false,
+    };
+
+    this.tasks.push(newTask);
+    this.nameTask = '';
+  }
+
+  //Delete a task
+  deleteTask(name: string): void {
+    const newTasks = this.tasks.filter((task) => task.name !== name);
+    this.tasks = newTasks;
+  }
+
+  //Update a task
+  updateTask(name: string): void {
+    const updateStateTask = this.tasks.map((task) => {
+      if ((task.name = name)) {
+        return {
+          ...task,
+          state: !task.state,
+        };
+      }
+      return task;
+    });
+
+    this.tasks = updateStateTask;
+  }
 }
